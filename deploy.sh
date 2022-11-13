78f66872b1017c9a464a60df7dac6037f5761d8b
Freeda Dsouza <fernandes.aaron1@gmail.com>
2022-11-13 16:57:01 +0530

    deploying the app to google cloud

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+docker build -t aaron2905/multi-client:latest -t aaron2905/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t aaron2905/multi-server:latest -t aaron2905/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t aaron2905/multi-worker:latest -t aaron2905/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push aaron2905/multi-client:latest
+docker push aaron2905/multi-server:latest
+docker push aaron2905/multi-worker:latest
+docker push aaron2905/multi-client:$SHA
+docker push aaron2905/multi-server:$SHA
+docker push aaron2905/multi-worker:$SHA
+kuebctl apply -f k8s
+kubectl set image deployments/server-deployment server=aaron2905/multi-server:$SHA
+kubectl set image deployments/client-deployment client=aaron2905/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=aaron2905/multi-worker:$SHA
